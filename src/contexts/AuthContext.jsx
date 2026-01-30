7178a37b610d1ba478d9f6e9742b38435a0acdc4
Yves <yvart.pro@gmail.com>
2026-01-30 19:11:57 +0200

    chore: Add ESLint disable directive for react-refresh/only-export-components on the `useAuth` hook.

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -54,6 +54,7 @@ export const AuthProvider = ({ children }) => {
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
