277ee1bee61ae928721d24f00a9b221f19549c9d
Yves <yvart.pro@gmail.com>
2026-01-31 06:14:31 +0200

    feat: Update Vite proxy path to `/api` and set `VITE_API_URL` to the production endpoint.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/kzrl/api': 'http://localhost:3000',
+      '/api': 'http://localhost:3000',
     },
   },
   base: "/kzrl",
